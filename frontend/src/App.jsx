a4fda75e492dd5f400735325937a05272ee223c1
Nehuen Lian <nehuenn.lian@gmail.com>
2026-03-24 07:50:41 -0300

    feat: add styles with Bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,21 @@ function App() {
   
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Ver tareas</Link>
-        <Link to="/task/create">Crear tarea</Link>
+      <nav className="py-3 mb-4 bg-dark border-bottom">
+        <div className="container">
+          <div className="row">
+            <div className="text-center">
+              <div className="d-flex gap-3 justify-content-center">
+                <button className="btn btn-primary text-white">
+                  <Link to="/" className="text-white">Ver tareas</Link>
+                </button>
+                <button className="btn btn-primary">
+                  <Link to="/task/create" className="text-white">Crear tarea</Link>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </nav>
 
       <Routes>
